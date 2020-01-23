8e39d5306dad54e4f8d2e8a3156dfd675b19f29d
alexandertippmann <alexti@uw.edu>
2020-01-23 16:21:49 +0100

    rotation implemented

--- game.js
+++ game.js
@@ -26,11 +26,15 @@ $("td").click(function(e){
   for(x=0;x<10;x++){
     for(y=0;y<20;y++){
       selectedPiece.pos[y][x] = selectedPiece.initpos[y][x];
-      selectedPiece.xMax = selectedPiece.xMaxInit;
-      selectedPiece.xMin = selectedPiece.xMinInit;
-      selectedPiece.yMax = selectedPiece.yMaxInit;
+
     }
   }
+  selectedPiece.xMax = selectedPiece.xMaxInit;
+  selectedPiece.xMin = selectedPiece.xMinInit;
+  selectedPiece.yMax = selectedPiece.yMaxInit;
+  selectedPiece.offsetX = 0;
+  selectedPiece.offsetY = 0;
+  selectedPiece.rotationCounter = 0;
   renderSelectedPiece(selectedPiece);
 });
 
@@ -41,6 +45,7 @@ $(document).keydown(function(ev){
     if(selectedPiece.xMin>0){
       selectedPiece.xMin--;
       selectedPiece.xMax--;
+      selectedPiece.offsetX--;
       for(x=0;x<10;x++){
         for(y=0;y<20;y++){
           if(selectedPiece.pos[y][x]==1){
@@ -58,6 +63,7 @@ $(document).keydown(function(ev){
     if(selectedPiece.xMax<9){
       selectedPiece.xMin++;
       selectedPiece.xMax++;
+      selectedPiece.offsetX++;
       for(x=9;x>=0;x--){
         for(y=0;y<20;y++){
           if(selectedPiece.pos[y][x]==1){
@@ -70,11 +76,41 @@ $(document).keydown(function(ev){
     renderSelectedPiece(selectedPiece);
     break;
     case "ArrowUp":
-    console.log("AU");
+    selectedPiece.rotationCounter++;
+    if(selectedPiece.rotationCounter==selectedPiece.rotationRule.length){
+      selectedPiece.rotationCounter=0;
+    }
+    selectedPiece.xMin += selectedPiece.rotX[selectedPiece.rotationCounter][0];
+    selectedPiece.xMax += selectedPiece.rotX[selectedPiece.rotationCounter][1];
+    selectedPiece.yMax += selectedPiece.rotX[selectedPiece.rotationCounter][2];
+    if(selectedPiece.xMax < 10 && selectedPiece.xMin>=0 && selectedPiece.yMax <20){
+      for(x=0;x<10;x++){
+        for(y=0;y<20;y++){
+          if(selectedPiece.pos[y][x]==1){
+            selectedPiece.pos[y][x]=-1;
+          }
+        }
+      }
+      for(x=0;x<10;x++){
+        for(y=0;y<20;y++){
+          if(selectedPiece.rotationRule[selectedPiece.rotationCounter][y][x]==1){
+            console.log(selectedPiece.rotationRule[selectedPiece.rotationCounter]);
+            selectedPiece.pos[y+selectedPiece.offsetY][x+selectedPiece.offsetX]=1;
+          }
+        }
+      }
+    }else{
+      selectedPiece.xMin -= selectedPiece.rotX[selectedPiece.rotationCounter][0];
+      selectedPiece.xMax -= selectedPiece.rotX[selectedPiece.rotationCounter][1];
+      selectedPiece.yMax -= selectedPiece.rotX[selectedPiece.rotationCounter][2];
+      selectedPiece.rotationCounter--;
+    }
+    renderSelectedPiece(selectedPiece);
     break;
     case "ArrowDown":
     if(selectedPiece.yMax <19){
       selectedPiece.yMax++;
+      selectedPiece.offsetY++;
       for(x=0;x<10;x++){
         for(y=19;y>=0;y--){
           if(selectedPiece.pos[y][x]==1){
@@ -86,9 +122,6 @@ $(document).keydown(function(ev){
     }
 
     renderSelectedPiece(selectedPiece);
-    break;
-    case "r":
-    console.log("rotate");
     break;
     default:
   }
--- pieceSetup.js
+++ pieceSetup.js
@@ -10,7 +10,7 @@ const green = "#2ecc71";
 const yellow = "#f1c40f";
 
 class Piece {
-  constructor(initialPosition, currentPosition, color, lowestX, highestX, highestY) {
+  constructor(initialPosition, currentPosition, color, lowestX, highestX, highestY, rotationRule, rotationX) {
     this.initpos = initialPosition;
     this.pos = currentPosition;
     this.color = color;
@@ -21,17 +21,33 @@ class Piece {
     this.xMaxInit = highestX;
     this.yMaxInit = highestY;
     this.rotationCounter = 0;
-    
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.rotationRule = rotationRule;
+    this.rotX = rotationX;
+
   }
 }
 
 var tArray = [];
+var tArrayRot1 = [];
+var tArrayRot2 = [];
+var tArrayRot3  = [];
 var iArray = [];
+var iArrayRot1 = [];
 var oArray = [];
 var sArray = [];
+var sArrayRot1= [];
 var zArray = [];
+var zArrayRot1 = [];
 var lArray = [];
+var lArrayRot1 = [];
+var lArrayRot2 = [];
+var lArrayRot3 = [];
 var llArray = [];
+var llArrayRot1 = [];
+var llArrayRot2 = [];
+var llArrayRot3 = [];
 var tArrayPos = [];
 var iArrayPos = [];
 var oArrayPos = [];
@@ -42,12 +58,24 @@ var llArrayPos = [];
 
 for (x = 0; x < 20; x++) {
   tArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  tArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  tArrayRot2.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  tArrayRot3.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   iArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  iArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   oArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   sArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  sArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   zArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  zArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   lArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  lArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  lArrayRot2.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  lArrayRot3.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   llArray.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  llArrayRot1.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  llArrayRot2.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  llArrayRot3.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   tArrayPos.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   iArrayPos.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   oArrayPos.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
@@ -60,10 +88,26 @@ tArray[0][4] = 1;
 tArray[1][3] = 1;
 tArray[1][4] = 1;
 tArray[1][5] = 1;
+tArrayRot1[0][4] = 1;
+tArrayRot1[1][3] = 1;
+tArrayRot1[1][4] = 1;
+tArrayRot1[2][4] = 1;
+tArrayRot2[1][3] = 1;
+tArrayRot2[1][4] = 1;
+tArrayRot2[1][5] = 1;
+tArrayRot2[2][4] = 1;
+tArrayRot3[0][4] = 1;
+tArrayRot3[1][4] = 1;
+tArrayRot3[2][4] = 1;
+tArrayRot3[1][5] = 1;
 iArray[0][5] = 1;
 iArray[1][5] = 1;
 iArray[2][5] = 1;
 iArray[3][5] = 1;
+iArrayRot1[0][3] = 1;
+iArrayRot1[0][4] = 1;
+iArrayRot1[0][5] = 1;
+iArrayRot1[0][6] = 1;
 oArray[0][4] = 1;
 oArray[0][5] = 1;
 oArray[1][4] = 1;
@@ -72,35 +116,73 @@ sArray[0][4] = 1;
 sArray[0][5] = 1;
 sArray[1][3] = 1;
 sArray[1][4] = 1;
+sArrayRot1[0][4] = 1;
+sArrayRot1[1][4] = 1;
+sArrayRot1[1][5] = 1;
+sArrayRot1[2][5] = 1;
 zArray[0][3] = 1;
 zArray[0][4] = 1;
 zArray[1][4] = 1;
 zArray[1][5] = 1;
+zArrayRot1[0][4] = 1;
+zArrayRot1[1][3] = 1;
+zArrayRot1[1][4] = 1;
+zArrayRot1[2][3] = 1;
 lArray[0][4] = 1;
 lArray[1][4] = 1;
 lArray[2][4] = 1;
 lArray[2][5] = 1;
+lArrayRot1[0][5] = 1;
+lArrayRot1[1][3] = 1;
+lArrayRot1[1][4] = 1;
+lArrayRot1[1][5] = 1;
+lArrayRot2[0][3] = 1;
+lArrayRot2[0][4] = 1;
+lArrayRot2[1][4] = 1;
+lArrayRot2[2][4] = 1;
+lArrayRot3[1][3] = 1;
+lArrayRot3[1][4] = 1;
+lArrayRot3[1][5] = 1;
+lArrayRot3[2][3] = 1;
 llArray[0][4] = 1;
 llArray[0][5] = 1;
 llArray[1][4] = 1;
 llArray[2][4] = 1;
+llArrayRot1[1][3] = 1;
+llArrayRot1[1][4] = 1;
+llArrayRot1[1][5] = 1;
+llArrayRot1[2][5] = 1;
+llArrayRot2[0][4] = 1;
+llArrayRot2[1][4] = 1;
+llArrayRot2[2][3] = 1;
+llArrayRot2[2][4] = 1;
+llArrayRot3[0][3] = 1;
+llArrayRot3[1][3] = 1;
+llArrayRot3[1][4] = 1;
+llArrayRot3[1][5] = 1;
 
-for (x = 0; x < 10; x++) {
-  for (y = 0; y < 20; y++) {
-    iArrayPos[y][x] = iArray[y][x];
-    oArrayPos[y][x] = oArray[y][x];
-    sArrayPos[y][x] = sArray[y][x];
-    zArrayPos[y][x] = zArray[y][x];
-    lArrayPos[y][x] = lArray[y][x];
-    llArrayPos[y][x] = llArray[y][x];
-  }
-}
 
-const tPiece = new Piece(tArray, tArrayPos, purple, 3, 5, 1);
-const iPiece = new Piece(iArray, iArrayPos, lightBlue, 5, 5, 3);
-const oPiece = new Piece(oArray, oArrayPos, yellow, 4, 5, 1);
-const sPiece = new Piece(sArray, sArrayPos, green, 3, 5, 1);
-const zPiece = new Piece(zArray, zArrayPos, red, 3, 5, 1);
-const lPiece = new Piece(lArray, lArrayPos, orange, 4, 5, 2);
-const llPiece = new Piece(llArray, llArrayPos, darkBlue, 4, 5, 2);
+
+const tArrayRotations = [tArray,tArrayRot1,tArrayRot2,tArrayRot3];
+const tArrayRot = [[-1,0,-1],[0,-1,1],[0,1,0],[1,0,0]];
+const iArrayRotations = [iArray,iArrayRot1];
+const iArrayRot =[[2,-1,3],[-2,1,-3]];
+const sArrayRotations = [sArray,sArrayRot1];
+const sArrayRot = [[-1,0,-1],[1,0,1]];
+const zArrayRotations = [zArray,zArrayRot1];
+const zArrayRot = [[0,1,-1],[0,-1,1]];
+const lArrayRotations = [lArray,lArrayRot1,lArrayRot2,lArrayRot3];
+const lArrayRot = [[1,0,0],[-1,0,-1],[0,-1,1],[0,1,0]];
+const llArrayRotations = [llArray,llArrayRot1,llArrayRot2,llArrayRot3];
+const llArrayRot = [[1,0,1],[-1,0,0],[0,-1,0],[0,1,-1]];
+const oArrayRotations = [oArray];
+const oArrayRot = [[0,0,0]];
+
+const tPiece = new Piece( tArray,  tArrayPos,  purple,    3, 5, 1, tArrayRotations,  tArrayRot);
+const iPiece = new Piece( iArray,  iArrayPos,  lightBlue, 5, 5, 3, iArrayRotations,  iArrayRot);
+const oPiece = new Piece( oArray,  oArrayPos,  yellow,    4, 5, 1, oArrayRotations , oArrayRot );
+const sPiece = new Piece( sArray,  sArrayPos,  green,     3, 5, 1, sArrayRotations , sArrayRot );
+const zPiece = new Piece( zArray,  zArrayPos,  red,       3, 5, 1, zArrayRotations , zArrayRot );
+const lPiece = new Piece( lArray,  lArrayPos,  orange,    4, 5, 2, lArrayRotations , lArrayRot );
+const llPiece = new Piece(llArray, llArrayPos, darkBlue,  4, 5, 2, llArrayRotations, llArrayRot  );
 const piecesArray = [tPiece, iPiece, oPiece, sPiece, zPiece, lPiece, llPiece];
